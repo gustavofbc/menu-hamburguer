d7d3cb108775cdd0324ee9c1d648c02fe7e8eefa
gustavofbc <gustavo.felipebc@gmail.com>
2021-12-15 18:42:08 -0300

    (ACESSIBILIDADE) implementaçao de ARIA nas tags

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
   if(event.type === "touchstart") event.preventDefault();
+
   const nav = document.getElementById('nav');
   nav.classList.toggle('ativo');
+  // ! ACESSIBILIDADE: serve para o leitor de HTML da página identificar quando o elemento pode ser expandido (se a classe ativo existir, significa que sim e está expadido, se não, permanece como "false")
+  const ativado = nav.classList.contains('ativo');
+  event.currentTarget.setAttribute('aria-expanded', ativado);
+  //  ! ACESSIBILIDADE: serve para alterar o texto do leitor para o botão. Se está aberto o botão terá o label de fechar, se está fechado o terá o valor de abrir.
+  if(ativado) {
+    event.currentTarget.setAttribute('aria-label', "Fechar menu")
+  } else {
+    event.currentTarget.setAttribute('aria-label', "Abrir menu")
+  }
+
 }
 
 btnMobile.addEventListener('click', toggleMenu);
