8d75cde072d53b219bacf307c8e8fdd29f6d1613
gustavofbc <gustavo.felipebc@gmail.com>
2021-12-15 16:40:53 -0300

    criaçao da estrutura e funcao base do js

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('ativo');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
